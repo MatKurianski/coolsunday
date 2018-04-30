c110bd9842eefaa836537316b45339fe9f7b9f4d
karacurt <karacurt@users.noreply.github.com>
2018-04-30 16:39:22 -0300

    server express funcionando

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './login';
-import servidor from './servidor';
-
-
- ReactDOM.render(<App />, document.getElementById('root'));
-servidor();
