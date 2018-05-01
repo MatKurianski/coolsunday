033211b570e75121b2d82975bc57e69a5397b03a
karacurt <karacurt@users.noreply.github.com>
2018-05-01 19:14:43 -0300

    login parcialmente funcionando

--- autenticacao.js
+++ autenticacao.js
@@ -1,18 +1,27 @@
 
 
-
-
 var express = require('express');
-var bodyParser = require('body-parser');
+
+
 var path = require('path');
 //var fs = require('fs');
 //target: 'node';
 
-var session;
+var session = require('express-session');
 
 var app = express();
 
+var cookieParser = require('cookie-parser');
+
+// load the cookie-parsing middleware
+var userID;
+
+
+app.use(cookieParser());
+
+var bodyParser = require('body-parser');
 app.use(bodyParser());
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(session({
@@ -23,28 +32,29 @@ saveUninitialized: true
 
 
 }));
+
 //app.user('/cssFiles', express;static(_dirname + '/assets'));
 
-app.get('/login', function (req, resp) {
-	resp.sendFile('login.js', {root: __dirname});
+app.get('/auth', function (req, resp) {
+	resp.sendFile('./Client/src/login.js', {root: __dirname});
 
 });
 
-app.post('/login', function(req, resp){
+app.post('/auth', function(req, resp){
 //	resp.end(JSON.stringify(req,body));
-session = req.session;
-
+session = req.sessionID;
+//userID = req.sessionID;
 	if(req.body.username === 'admin' && req.body.password === 'admin'){
-		session.uniqueID = req.body.username;
+		session = req.body.username;
 	}
 resp.redirect('/redirects');
 });
 
 app.get('/redirects', function(req, resp){
-	session = req.sessions;
-if(session.uniqueID){
-	console.log(session.uniqueID);
-resp.redirect('/admin');
+	session = req.sessionID;
+if(session){
+	console.log(session);
+resp.redirect('http://localhost:3000/logou');
 }else{
 	resp.end('Who are you?');
 }
